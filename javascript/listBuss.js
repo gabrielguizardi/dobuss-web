e475eb56ff07a124da241761386414839a300ff2
Gabriel Guizardi <gabrielguicar55@gmail.com>
2021-11-23 14:38:00 -0300

    Finalizando o web

--- javascript/listBuss.js
+++ javascript/listBuss.js
@@ -1,57 +1,23 @@
-data = [
-  {
-    id: 2,
-    buss_id: 1,
-    created_at: "2021-11-21 17:55:30",
-    updated_at: "2021-11-21 17:55:30",
-    buss: {
-      id: 1,
-      model: "1001 - R KING",
-      brand: "Mercedes",
-      plate: "RKNG 1001",
-      created_at: "2021-11-21 16:51:06",
-      updated_at: "2021-11-21 16:51:06"
-    }
-  },
-  {
-    id: 3,
-    buss_id: 1,
-    created_at: "2021-11-22 11:49:04",
-    updated_at: "2021-11-22 11:49:04",
-    buss: {
-      id: 1,
-      model: "1001 - R KING",
-      brand: "Mercedes",
-      plate: "RKNG 1001",
-      created_at: "2021-11-21 16:51:06",
-      updated_at: "2021-11-21 16:51:06"
-    }
-  }
-]
-
-
-
-
-
 window.onload = function() {
-  listBuss ()
+  requestApi()
 };
 
 function requestApi() {
   $.ajax({
-    url: 'url',
-    type: 'POST',
+    url: 'http://localhost:3333/busses',
+    type: 'GET',
     dataType: 'json',
     crossDomain: true,
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*'
-    },
-    data: body
+    }
   })
   .then(function(res) {
+    // res.data.body
     console.log(res)
+    listBuss(res)
   })
   .catch(function(err) {                
     console.log(err)
@@ -62,8 +28,8 @@ function showBuss(value) {
   $(location).prop('href', `show.html?id=${value}`)
 }
 
-function listBuss () {
+function listBuss (data) {
   data.length === 0 ? 
   $('#show-buss').append("<h1 style='text-align: center'>Nenhuma rota foi encontrado</h1>") :  
-  data.map((value) => $('#show-buss').append(`<div class='table-row mb-20' onclick='showBuss(${value.id})'><p>${value.id}</p><p>${value.buss.id}</p><p>${value.buss.model}</p><p>${value.buss.brand}</p><p>${value.buss.plate}</p></div>`)) 
+  data.map((value) => $('#show-buss').append(`<div class='table-row mb-20' onclick='showBuss(${value.routes[0].id})'><p>${value.id}</p><p>${value.model}</p><p>${value.brand}</p><p>${value.plate}</p></div>`)) 
 }
